2b8f0ef84e9e6f4c7b7c54299309519419aaafcf
Terry Finkel <finkelterry@flerken.io>
2019-09-12 14:46:29 +0200

    Add even more CSS

--- client/src/App.vue
+++ client/src/App.vue
@@ -30,6 +30,17 @@
                     </span>
                     <Generator :disabled="disableUpdate" @click="updateMatrix"/>
                 </div>
+                <div v-if="solutionPath" class="results-wrapper">
+                    <b-input-group prepend="Solved in">
+                        <b-form-input v-model="solvingTime" class="text-right" :readonly=true prefix="test"/>
+                    </b-input-group>
+                    <b-input-group prepend="Total number of states selected">
+                        <b-form-input v-model="totalStates" class="text-right" :readonly=true prefix="test"/>
+                    </b-input-group>
+                    <b-input-group prepend="Maximum number of states in memory">
+                        <b-form-input v-model="maxStates" class="text-right" :readonly=true prefix="test"/>
+                    </b-input-group>
+                </div>
             </div>
 
             <!-- Right side with grid and solution -->
@@ -41,7 +52,7 @@
                         :path="solutionPath"
                         :solving="solving"
                         @play="disableUpdate = true"
-                        @reset="reset"
+                        @reset="resetMatrix"
                         @solve="solve"
                         @stop="disableUpdate = false"
                         @unsolve="unsolve"
@@ -85,11 +96,14 @@ export default {
             heuristic: 'hamming',
             initialMatrix: [[1, 2, 3], [8, 0, 4], [7, 6, 5]],
             matrix: [[1, 2, 3], [8, 0, 4], [7, 6, 5]],
+            maxStates: 0,
             moves: 0,
             solutionPath: '',
             solving: false,
+            solvingTime: '',
             search: 'greedy',
-            serverOnline: false
+            serverOnline: false,
+            totalStates: 0
         }
     },
     methods: {
@@ -118,6 +132,13 @@ export default {
             });
         },
         reset () {
+            this.moves = 0;
+            this.maxStates = 0;
+            this.totalStates = 0;
+            this.solvingTime = '';
+            this.solutionPath = '';
+        },
+        resetMatrix () {
             this.matrix = this.initialMatrix.map(row => row.slice());
         },
         solve () {
@@ -142,20 +163,21 @@ export default {
                     } else if (res.getSuccess() == false) {
                         alert(res.getError());
                     } else {
+                        this.maxStates = res.getMaxstates();
+                        this.totalStates = res.getTotalstates();
                         this.moves = res.getMoves();
+                        this.solvingTime = res.getTime();
                         this.solutionPath = res.getPath();
                     }
                 })
             }
         },
         unsolve () {
+            this.reset();
             this.matrix = this.initialMatrix.map(row => row.slice());
-            this.moves = '';
-            this.solutionPath = '';
         },
         updateMatrix (matrix) {
-            this.moves = 0;
-            this.solutionPath = '';
+            this.reset();
             this.matrix = matrix;
             this.initialMatrix = matrix.map(row => row.slice());
         },
@@ -174,15 +196,27 @@ export default {
         justify-content: center;
     }
 
+    .navbar {
+        margin-left: -20%;
+        width: 140%;
+    }
+
     .options-wrapper {
         display: flex;
         justify-content: center;
-        margin-bottom: 2vh;
         margin-top: 2vh;
+        width: 125%;
+        margin-left: -12.5%;
     }
 
     .picker-wrapper {
         justify-content: space-around;
+        margin-top: 2vh;
+        margin-bottom: 2vh;
+    }
+
+    .results-wrapper {
+
     }
 
     .status-wrapper {
@@ -196,11 +230,6 @@ export default {
         -moz-osx-font-smoothing: grayscale;
         text-align: center;
         color: #2c3e50;
-    }
-
-    .navbar {
-        margin-left: -20%;
-        width: 140%;
     }
 
     #status {
--- client/src/components/Options.vue
+++ client/src/components/Options.vue
@@ -1,7 +1,13 @@
 <template>
     <div class="options">
         <b-input-group prepend="Heuristic function">
-            <b-form-select class="text-center" v-model="heuristic" :options="heuristicOptions" @change="onUpdate"/>
+            <b-form-select
+                    class="text-center"
+                    v-b-tooltip="test"
+                    v-model="heuristic"
+                    :options="heuristicOptions"
+                    @change="onUpdate"
+            />
         </b-input-group>
         <b-input-group prepend="A* search variant">
             <b-form-select class="text-center" v-model="search" :options="searchOptions" @change="onUpdate"/>
@@ -19,12 +25,12 @@ export default {
                 { value: 'manhattan', text: 'Manhattan Distance' },
                 { value: 'manhattan + linear conflicts', text: 'Manhattan Distance + Linear Conflicts' }
             ],
-            heuristic: 'hamming',
+            heuristic: 'manhattan + linear conflicts',
             searchOptions: [
                 { value: 'greedy', text: 'Greedy Search' },
                 { value: 'uniform-cost', text: 'Uniform-Cost Search' }
             ],
-            search: 'greedy'
+            search: 'uniform-cost'
         }
     },
     methods: {
